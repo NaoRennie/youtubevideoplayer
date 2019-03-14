2bee282ac329351b62ca3db139bd6e21ab604f36
NaomiRennie <naomi.lily.405@gmail.com>
2019-03-14 10:48:09 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <div class="row">
+            <VideoDetail :videoToPlay="this.selectedVideo1"/>
+            <VideoList @videoSelected="onVideoSelectedToPlay" :videoList="videos"></VideoList>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
+const API_KEY = ''
+export default {
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList,
+        VideoDetail
+    },
+    data() {
+        return { videos: [], selectedVideo1: null }
+    },
+    methods: {
+       onVideoSelectedToPlay(video) {
+            this.selectedVideo1 = video;
+            /* eslint-disable no-console */
+            console.log(video,'video')
+        },
+        onTermChange(searchTerm) {
+              /* eslint-disable no-console */
+              console.log(searchTerm)
+              axios.get('https://www.googleapis.com/youtube/v3/search',{
+                  params: {
+                      key: API_KEY,
+                      type: 'video',
+                      part: 'snippet',
+                      q: searchTerm
+                  }
+              })
+              .then(response => {
+                  this.videos = response.data.items;
+              });
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="list-group col-md-4">
+       <VideoListItem 
+        v-for="video in videoList"
+        :singleVideo="video"
+        :key="video.etag"
+        @videoSelect="onVideoSelected"
+        >
+       </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem'
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: {
+        videoList: Array
+    },
+    methods: {
+        onVideoSelected(video) {
+            this.$emit('videoSelected', video)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
